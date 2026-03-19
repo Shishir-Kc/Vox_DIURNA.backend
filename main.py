9217e8a0aa3a928a9058f8e2a53dbd15fa0141d7
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-03-19 19:00:36 +0545

    added my domain

--- main.py
+++ main.py
@@ -15,27 +15,35 @@ async def lifespan(server: FastAPI):
 
 
 server = FastAPI(
-    title="VOX_DIURNA", version="1.0", lifespan=lifespan, docs_url=None, redoc_url=None
+    title="VOX_DIURNA", version="1.0", lifespan=lifespan, docs_url="/", redoc_url=None
 )
 
 server.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://vox-diurna.pages.dev",
-        "http://localhost",
-        "http://localhost:3000",
-        "http://localhost:5173",
+        "https://shishirkhatri.com.np",
+        # "http://localhost",
+        # "http://localhost:3000",
+        # "http://localhost:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 server.add_middleware(
-    TrustedHostMiddleware, allowed_hosts=["vox-diurna-backend.onrender.com", "*.onrender.com", "localhost"]
+    TrustedHostMiddleware,
+    allowed_hosts=[
+        "vox-diurna-backend.onrender.com",
+        "*.onrender.com",
+        # "localhost",
+        # "127.0.0.1",
+        # "localhost:8000",
+        # "127.0.0.1:8000",
+    ],
 )
 
 server.state.limiter = limiter
 server.add_middleware(SlowAPIASGIMiddleware)
 server.add_exception_handler(429, _rate_limit_exceeded_handler)  # pyright: ignore[]
 server.include_router(router, prefix="/api")
-
