d752ecf272075ed5c3ff4a5e00e7bb37cf1c30bb
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-04-11 15:44:02 +0545

    Fix SSL connection: add sslmode=require to database engine

--- database/connnection/connector.py
+++ database/connnection/connector.py
@@ -1,16 +1,18 @@
-from sqlmodel import create_engine,Session
+from sqlmodel import create_engine, Session
 from typing import Annotated
 from fastapi import Depends
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-db_url=os.getenv("DATABASE_URL")
-engine = create_engine(url=db_url)
+db_url = os.getenv("DATABASE_URL")
+engine = create_engine(url=db_url, connect_args={"sslmode": "require"})
+
 
 def get_sesison():
     with Session(engine) as session:
         yield session
 
-Session_Dep = Annotated[Session,Depends(get_sesison)]
 
+Session_Dep = Annotated[Session, Depends(get_sesison)]
